31e20e96b528abaaef7ac056dc8b98396f1b0467
Shayne Boyer <shayne.p.boyer@disney.com>
2015-10-27 08:33:40 -0700

    .

--- app.js
+++ app.js
@@ -5,39 +5,7 @@ var express = require('express'),
 var address_url = 'http://peopleservice.azurewebsites.net/Address.svc?wsdl';
 var people_url = 'http://peopleservice.azurewebsites.net/People.svc?wsdl';
 
-var args = { value: 5 };
-
-app.get('/address', function (req, res) {
-
-    soap.createClient(address_url, function (err, client) {
-        client.GetAddressData(args, function (err, result) {
-
-            if (!err)
-                console.log(err);
-
-            console.log(JSON.stringify(result.GetAddressDataResult.Address[0]));
-
-            res.send(result.GetAddressDataResult);
-        });
-    });
-});
-
-
-app.get('/people', function (req, res) {
-
-    soap.createClient(people_url, function (err, client) {
-        client.GetPeopleData(args, function (err, result) {
-
-            if (!err)
-                console.log(err);
-
-            console.log(JSON.stringify(result.GetPeopleDataResult.Person[0]));
-
-            res.send(result.GetPeopleDataResult);
-        });
-    });
-});
-
+var args = { value: 25 };
 
 app.get('/allpeople', function (req, res) {
     soap.createClient(people_url, function (err, client) {
